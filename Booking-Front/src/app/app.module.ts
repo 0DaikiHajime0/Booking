6deb73403c41218021d4805b435f13ff0d407089
humanbotgit <121126539+humanbotgit@users.noreply.github.com>
2024-04-09 16:19:22 -0500

    Actualización de angular material

--- Booking-Front/src/app/app.module.ts
+++ Booking-Front/src/app/app.module.ts
@@ -5,11 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 import { LoginComponent } from './login/login.component';
 import { PerfilComponent } from './seccion/perfil/perfil.component';
 import { CrearReservaComponent } from './seccion/crear-reserva/crear-reserva.component';
 import { MostrarReservasComponent } from './seccion/mostrar-reservas/mostrar-reservas.component';
 import { HomeComponent } from './home/home.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
 
@@ -25,10 +28,13 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
