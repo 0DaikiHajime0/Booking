ef40020c3f16d1379772a7f986f0b1c3b1d03a7b
humanbotgit <121126539+humanbotgit@users.noreply.github.com>
2024-04-09 08:32:23 -0500

    Angular material y cdk
    Angular material y cdk

--- Booking-Front/src/app/app.module.ts
+++ Booking-Front/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
   ],
   providers: [
     provideClientHydration()
