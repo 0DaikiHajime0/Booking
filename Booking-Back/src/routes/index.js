fd7c0da1b75b9cf5a8b98c4234a78845871d243c
0DaikiHajime0 <113397401+0DaikiHajime0@users.noreply.github.com>
2024-04-19 18:17:52 -0500

    listarReserva

--- Booking-Back/src/routes/index.js
+++ Booking-Back/src/routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-//const listarReserva = require('./listarReserva.routes');
 const crearReservaRoute = require('./crearReserva.routes');
 const usuarioRoute = require('./usuario.routes')
+const listarReserva = require('./listarReserva.routes')
 
 function routerApi(app){
   const route = express.Router();
   app.use(express.json());
   app.use('/api/v1', route);
 
- // route.use('/listar', listarReserva);
+  route.use('/listar', listarReserva);
   route.use('/reserva', crearReservaRoute);
   route.use('/usuario',usuarioRoute)
 }
