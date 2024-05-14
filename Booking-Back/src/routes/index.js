82a64490b58171fad504f8bff43d1b34457e6696
0DaikiHajime0 <113397401+0DaikiHajime0@users.noreply.github.com>
2024-05-14 09:02:31 -0500

    eeeeeooooo

--- Booking-Back/src/routes/index.js
+++ Booking-Back/src/routes/index.js
@@ -8,12 +8,14 @@ const bloqueRoute = require('./bloque.routes')
 const verificarToken = require('../../middlewares/auth')
 const authRoute =require('../routes/auth.routes')
 function routerApi(app){
+  
   const route = express.Router();
+
   app.use(express.json());
   app.use('/api/v1', route);
   route.use('/auth',authRoute);
   route.use(verificarToken);
-  route.use('/usuario',usuarioRoute);  
+  route.use('/usuario',usuarioRoute);
   route.use('/listar', listarReserva);
   route.use('/reserva', crearReservaRoute);
   route.use('/recurso',recursoRoute);
