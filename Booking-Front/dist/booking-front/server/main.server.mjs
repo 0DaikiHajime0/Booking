b9b6270de15aeb1868d7e2a8e4b91c8e4ce8b1c1
humanbotgit <jarancibiasedano@gmail.com>
2025-02-20 15:59:13 -0500

    icon changed

--- Booking-Front/dist/booking-front/server/main.server.mjs
+++ Booking-Front/dist/booking-front/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WRQOQ472.mjs";import"./chunk-IXLBNLYY.mjs";import"./chunk-P37ZEURT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-QEX5OMCA.mjs";import"./chunk-IXLBNLYY.mjs";import"./chunk-P37ZEURT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
