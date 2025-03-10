952f8fb118b7b764beff51ffc4b41aef105a5b45
0DaikiHajime0 <113397401+0DaikiHajime0@users.noreply.github.com>
2025-03-10 16:49:05 -0500

    SQL Update

--- Booking-Back/libs/mysql.js
+++ Booking-Back/libs/mysql.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'booking',
-  port: 3306,
+  user: 'devoewsq_kardex',
+  password: 'recursosvirt1',
+  database: 'devoewsq_booking',
+  port: '3306'
 });
 module.exports =pool.promise();
